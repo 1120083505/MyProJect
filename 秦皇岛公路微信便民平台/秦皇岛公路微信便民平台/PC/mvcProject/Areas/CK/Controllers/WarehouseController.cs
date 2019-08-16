b2dddf14b03cd016958ef8b8fc35766c5237a8cf
ZhuChangWei <1120083505@qq.com>
2019-08-16 17:40:18 +0800

    修改格式

--- 秦皇岛公路微信便民平台/秦皇岛公路微信便民平台/PC/mvcProject/Areas/CK/Controllers/WarehouseController.cs
+++ 秦皇岛公路微信便民平台/秦皇岛公路微信便民平台/PC/mvcProject/Areas/CK/Controllers/WarehouseController.cs
@@ -1,107 +1,216 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Core;
+using Microsoft.Reporting.WebForms;
 using Models;
 using SubSonic;
 using ZrSoft;
-using Core;
-using System.Data;
-using System.IO;
-using Microsoft.Reporting.WebForms;
-using System.Text;
+
 namespace mvcProject.Areas.CK.Controllers
 {
     public class WarehouseController : Controller
     {
+        private readonly ControllerHelper c = new ControllerHelper();
+        private string LogStr = "";
         public string msg = "";
+        private readonly QueryPaged query = new QueryPaged();
         public int status = 1;
-        string LogStr = "";
-        ControllerHelper c = new ControllerHelper();
-        QueryPaged query = new QueryPaged();
+
         [PowerFilter]
         public ActionResult Index()
         {
             return View();
         }
+
         [PowerFilter]
         public ActionResult Info(string id)
         {
-            CK_Warehouse entity = new CK_Warehouse(id);
+            var entity = new CK_Warehouse(id);
             entity.AdminId = new Sys_Admin(entity.AdminId).RealName;
             return View(entity);
         }
+
         [HttpPost]
         [LoginPower]
         public JsonResult GetList(GridPager pager, string queryStr)
         {
             if (pager.rows == 0)
                 pager.rows = Core.Config.PageSize;
-            List<QueryModel> qList = c.getQList(queryStr);
+            var qList = c.getQList(queryStr);
             query.pager = pager;
             query.TableName = CK_Warehouse.Schema;
             query.init();
             GetQuery(qList);
-            int total = 0;
-            List<CK_Warehouse> list = query.Paged(ref total).ExecuteTypedList<CK_Warehouse>();
+            var total = 0;
+            var list = query.Paged(ref total).ExecuteTypedList<CK_Warehouse>();
             var json = new
             {
-                total = total,
-                pager = query.pager,
+                total,
+                query.pager,
                 rows = list.Select((r, i) => new
                 {
-                    RowNumber = (i++) + (pager.page - 1) * pager.rows + 1,
+                    RowNumber = i++ + (pager.page - 1) * pager.rows + 1,
                     r.Id,
                     r.Name,
                     r.Area,
                     r.Ardess,
                     r.Stock,
                     r.Memo,
-                    AdminId =new Sys_Admin( r.AdminId).RealName,
+                    AdminId = new Sys_Admin(r.AdminId).RealName,
                     Pubdate = string.Format("{0:yyyy-MM-dd}", r.Pubdate),
                     r.Ext1,
                     r.Ext2,
                     r.Ext3
-
                 })
             };
             return Json(json);
         }
+
         [HttpPost]
         public JsonResult GetAllList()
         {
-            SqlQuery sq = new Select().From<CK_Warehouse>();
-            List<CK_Warehouse> list = sq.ExecuteTypedList<CK_Warehouse>();
+            var sq = new Select().From<CK_Warehouse>();
+            var list = sq.ExecuteTypedList<CK_Warehouse>();
             var json = new
             {
                 total = list.Count,
                 rows = (from r in list
                         select new
                         {
-                            Id = r.Id,
-                            Name = r.Name,
-                            Area = r.Area,
-                            Ardess = r.Ardess,
-                            Stock = r.Stock,
-                            Memo = r.Memo,
-                            AdminId = r.AdminId,
+                            r.Id,
+                            r.Name,
+                            r.Area,
+                            r.Ardess,
+                            r.Stock,
+                            r.Memo,
+                            r.AdminId,
                             Pubdate = string.Format("{0:yyyy-MM-dd}", r.Pubdate),
-                            Ext1 = r.Ext1,
-                            Ext2 = r.Ext2,
-                            Ext3 = r.Ext3
-
+                            r.Ext1,
+                            r.Ext2,
+                            r.Ext3
                         }
-              ).ToArray()
+                    ).ToArray()
             };
             return Json(json);
         }
+
+        #region Delete  
+
+        [HttpPost]
+        public JsonResult Delete(string id)
+        {
+            if (!string.IsNullOrWhiteSpace(id))
+                try
+                {
+                    //这里书写需要删除的逻辑代码 
+                    new Delete().From<CK_Warehouse>().Where(CK_Warehouse.Columns.Id).In(id.Split(','))
+                        .Execute();
+                    status = 1;
+                    msg = Tip.DeleteSucceed;
+                }
+                catch (Exception e)
+                {
+                    status = 0;
+                    msg = Tip.DeleteFail + Utils.NoHTML(e.Message);
+                }
+
+            GUI.InsertLog(" 仓库", "删除", msg + LogStr);
+            return ControllerHelper.jsonresult(status, msg);
+        }
+
+        #endregion
+
+        #region 导出到PDF EXCEL WORD 
+
+        [PowerFilter]
+        public ActionResult Export(string type, string queryStr)
+        {
+            var pager = new GridPager();
+            var qList = c.getQList(queryStr);
+            query.pager = pager;
+            query.TableName = CK_Warehouse.Schema;
+            query.init();
+            GetQuery(qList);
+            var list = query.Paged().ExecuteTypedList<CK_Warehouse>();
+            var localReport = new LocalReport();
+            try
+            {
+                localReport.ReportPath = Server.MapPath("~/Report/CK_Warehouse.rdlc");
+                var reportDataSource = new ReportDataSource("DataSet", list);
+                localReport.DataSources.Add(reportDataSource);
+                if (type.ToLower() == "print")
+                {
+                    //直接打印
+                    ReportPrint.Run(localReport);
+                    return View("/views/export/print.cshtml");
+                }
+
+                var reportType = type;
+                string mimeType;
+                string encoding;
+                string fileNameExtension;
+
+                var deviceInfo =
+                    "<DeviceInfo>" +
+                    "<OutPutFormat>" + type + "</OutPutFormat>" +
+                    "<PageWidth>11in</PageWidth>" +
+                    "<PageHeight>11in</PageHeight>" +
+                    "<MarginTop>0.5in</MarginTop>" +
+                    "<MarginLeft>1in</MarginLeft>" +
+                    "<MarginRight>1in</MarginRight>" +
+                    "<MarginBottom>0.5in</MarginBottom>" +
+                    "</DeviceInfo>";
+                Warning[] warnings;
+                string[] streams;
+                byte[] renderedBytes;
+
+                renderedBytes = localReport.Render(
+                    reportType,
+                    deviceInfo,
+                    out mimeType,
+                    out encoding,
+                    out fileNameExtension,
+                    out streams,
+                    out warnings
+                );
+                var ExportName = " 仓库";
+                GUI.InsertLog(" 仓库", "导出", "导出[" + ExportName + "]成功！[" + type.ToLower() + "]");
+                switch (type.ToLower())
+                {
+                    case "pdf":
+                        ExportName += ".pdf";
+                        break;
+                    case "image":
+                        ExportName += ".png";
+                        break;
+                    case "excel":
+                        ExportName += ".xls";
+                        break;
+                    case "word":
+                        ExportName += ".doc";
+                        break;
+                }
+
+                return File(renderedBytes, mimeType, ExportName);
+            }
+            catch (Exception ex)
+            {
+                return View("/views/export/error.cshtml");
+            }
+        }
+
+        #endregion
+
         #region 拼接查询条件  
+
         //拼接查询条件  
         public void GetQuery(List<QueryModel> qList)
         {
             GetQuery(qList, null);
         }
+
         public void GetQuery(List<QueryModel> qList, string[] coulums)
         {
             if (c.GetQueryStr(qList, "searchtype") == "0")
@@ -116,7 +225,6 @@ namespace mvcProject.Areas.CK.Controllers
                 query.IsEqualTo(qList, CK_Warehouse.Columns.Ext1);
                 query.IsEqualTo(qList, CK_Warehouse.Columns.Ext2);
                 query.IsEqualTo(qList, CK_Warehouse.Columns.Ext3);
-
             }
             else
             {
@@ -131,12 +239,14 @@ namespace mvcProject.Areas.CK.Controllers
                 query.Like(qList, CK_Warehouse.Columns.Ext2);
                 query.Like(qList, CK_Warehouse.Columns.Ext3);
             }
+
             query.IsEqualTo(qList, CK_Warehouse.Columns.Area);
             //query.IsGreaterThanOrEqualTo(qList, CK_Warehouse.Columns.Pubdate);  
             //query.IsLessThanOrEqualTo(qList, CK_Warehouse.Columns.Pubdate);  
-
         }
+
         #endregion
+
         #region Create  
 
         [PowerFilter]
@@ -144,6 +254,7 @@ namespace mvcProject.Areas.CK.Controllers
         {
             return View();
         }
+
         [HttpPost]
         [PowerFilter]
         public JsonResult Create(CK_Warehouse model)
@@ -162,39 +273,47 @@ namespace mvcProject.Areas.CK.Controllers
                 status = 0;
                 msg = Tip.InsertFail;
             }
-            GUI.InsertLog(" 仓库", "新增", msg + ":" + model.Id + ",仓库名称:" + model.Name + ",面积:" + model.Area + ",地址:" + model.Ardess + ",库存:" + model.Stock + ",备注:" + model.Memo + ",录入人:" + model.AdminId + ",录入时间:" + model.Pubdate + ",:" + model.Ext1 + ",:" + model.Ext2 + ",:" + model.Ext3 + "");
+
+            GUI.InsertLog(" 仓库", "新增",
+                msg + ":" + model.Id + ",仓库名称:" + model.Name + ",面积:" + model.Area + ",地址:" + model.Ardess + ",库存:" +
+                model.Stock + ",备注:" + model.Memo + ",录入人:" + model.AdminId + ",录入时间:" + model.Pubdate + ",:" +
+                model.Ext1 + ",:" + model.Ext2 + ",:" + model.Ext3 + "");
             return ControllerHelper.jsonresult(status, msg);
         }
+
         #endregion
+
         #region Edit  
+
         [PowerFilter]
         public ActionResult Edit(string id)
         {
-            CK_Warehouse entity = new CK_Warehouse(id);
+            var entity = new CK_Warehouse(id);
             return View(entity);
         }
+
         [HttpPost]
         [PowerFilter]
         public JsonResult Edit(CK_Warehouse model)
         {
             try
             {
-                CK_Warehouse entity = new CK_Warehouse(model.Id);
+                var entity = new CK_Warehouse(model.Id);
                 if (!string.IsNullOrEmpty(entity.Id))
                 {
                     LogStr = ""
-                 + "Id:" + model.Id
-                 + ",仓库名称:" + model.Name
-                 + ",面积:" + model.Area
-                 + ",地址:" + model.Ardess
-                 + ",库存:" + model.Stock
-                 + ",备注:" + model.Memo
-                 + ",录入人:" + model.AdminId
-                 + ",录入时间:" + model.Pubdate
-                 + ",:" + model.Ext1
-                 + ",:" + model.Ext2
-                 + ",:" + model.Ext3
-                    + "";
+                             + "Id:" + model.Id
+                             + ",仓库名称:" + model.Name
+                             + ",面积:" + model.Area
+                             + ",地址:" + model.Ardess
+                             + ",库存:" + model.Stock
+                             + ",备注:" + model.Memo
+                             + ",录入人:" + model.AdminId
+                             + ",录入时间:" + model.Pubdate
+                             + ",:" + model.Ext1
+                             + ",:" + model.Ext2
+                             + ",:" + model.Ext3
+                             + "";
                     entity.Name = model.Name;
                     entity.Area = model.Area;
                     entity.Ardess = model.Ardess;
@@ -220,114 +339,11 @@ namespace mvcProject.Areas.CK.Controllers
                 status = 0;
                 msg = Tip.EditFail + Utils.NoHTML(e.Message);
             }
+
             GUI.InsertLog(" 仓库", "编辑", msg + LogStr);
             return ControllerHelper.jsonresult(status, msg);
         }
-        #endregion
-        #region Delete  
-        [HttpPost]
-        public JsonResult Delete(string id)
-        {
-            if (!string.IsNullOrWhiteSpace(id))
-            {
-                try
-                {
-                    //这里书写需要删除的逻辑代码 
-                    new Delete().From<CK_Warehouse>().Where(CK_Warehouse.Columns.Id).In(id.Split(','))
-                    .Execute();
-                    status = 1;
-                    msg = Tip.DeleteSucceed;
-                }
-                catch (Exception e)
-                {
-                    status = 0;
-                    msg = Tip.DeleteFail + Utils.NoHTML(e.Message);
-                }
-            }
-            else
-            {
 
-            }
-            GUI.InsertLog(" 仓库", "删除", msg + LogStr);
-            return ControllerHelper.jsonresult(status, msg);
-        }
-        #endregion
-        #region 导出到PDF EXCEL WORD 
-        [PowerFilter]
-        public ActionResult Export(string type, string queryStr)
-        {
-            GridPager pager = new GridPager();
-            List<QueryModel> qList = c.getQList(queryStr);
-            query.pager = pager;
-            query.TableName = CK_Warehouse.Schema;
-            query.init();
-            GetQuery(qList);
-            List<CK_Warehouse> list = query.Paged().ExecuteTypedList<CK_Warehouse>();
-            LocalReport localReport = new LocalReport();
-            try
-            {
-                localReport.ReportPath = Server.MapPath("~/Report/CK_Warehouse.rdlc");
-                ReportDataSource reportDataSource = new ReportDataSource("DataSet", list);
-                localReport.DataSources.Add(reportDataSource);
-                if (type.ToLower() == "print")
-                {
-                    //直接打印
-                    ReportPrint.Run(localReport);
-                    return View("/views/export/print.cshtml");
-                }
-
-                string reportType = type;
-                string mimeType;
-                string encoding;
-                string fileNameExtension;
-
-                string deviceInfo =
-                    "<DeviceInfo>" +
-                    "<OutPutFormat>" + type + "</OutPutFormat>" +
-                    "<PageWidth>11in</PageWidth>" +
-                    "<PageHeight>11in</PageHeight>" +
-                    "<MarginTop>0.5in</MarginTop>" +
-                    "<MarginLeft>1in</MarginLeft>" +
-                    "<MarginRight>1in</MarginRight>" +
-                    "<MarginBottom>0.5in</MarginBottom>" +
-                    "</DeviceInfo>";
-                Warning[] warnings;
-                string[] streams;
-                byte[] renderedBytes;
-
-                renderedBytes = localReport.Render(
-                    reportType,
-                    deviceInfo,
-                    out mimeType,
-                    out encoding,
-                    out fileNameExtension,
-                    out streams,
-                    out warnings
-                    );
-                string ExportName = " 仓库";
-                GUI.InsertLog(" 仓库", "导出", "导出[" + ExportName + "]成功！[" + type.ToLower() + "]");
-                switch (type.ToLower())
-                {
-                    case "pdf":
-                        ExportName += ".pdf";
-                        break;
-                    case "image":
-                        ExportName += ".png";
-                        break;
-                    case "excel":
-                        ExportName += ".xls";
-                        break;
-                    case "word":
-                        ExportName += ".doc";
-                        break;
-                }
-                return File(renderedBytes, mimeType, ExportName);
-            }
-            catch (Exception ex)
-            {
-                return View("/views/export/error.cshtml");
-            }
-        }
         #endregion
     }
 }
